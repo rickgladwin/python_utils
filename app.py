9a6fbe3823c35900ec3a42124b7fc3d2356ee10a
Rick Gladwin <rick@iddesign.ca>
2021-10-21 21:15:06 -0400

    add app.py (main file)

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# A Flask app that runs a microservice on the local machine providing
+# access to python utilities via API endpoints.
+#
+# To run the service, `flask run` on the command line.
+
 from flask import Flask
 
 app = Flask(__name__)
