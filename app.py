395e385ff2613a8b4020c2e591438a26f3c35460
Rick Gladwin <rick@iddesign.ca>
2021-12-05 18:32:19 -0500

    refactor functions in app.py to use build_data_object

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from modules.combinations import n_choose_r
 
 from flask import Flask, request, Response
 
+# change this line if you want to use a different file as the root file for the Flask app
 app = Flask(__name__)
 
 
@@ -45,17 +46,17 @@ def add():
 
 
 @app.route('/primes/nth_prime', methods=['GET'])
-def primes_nth_prime():
+def primes__nth_prime():
     primes = Primes()
     n:int = int(request.args.get('n'))
+
     nth_prime:int = primes.nth_prime_number(n)
 
     return build_data_object(nth_prime)
-    # return str(nth_prime)
 
 
 @app.route('/partitional/partition_set', methods=['GET'])
-def partitional_partition_set():
+def partitional__partition_set():
     # e.g. http://127.0.0.1:5000/partitional/partition_set?source_list=hello|there|world&partitions_count=2
     # with json objects: http://127.0.0.1:5000/partitional/partition_set?source_list={%22id%22:1,%22body%22:%22hello%22}|{%22id%22:2,%22body%22:%22there%22}|{%22id%22:3,%22body%22:%22world%22}&partitions_count=2
     source_list: list = request.args.get('source_list').split('|')
@@ -67,16 +68,20 @@ def partitional_partition_set():
 
 
 @app.route('/combinations/n_choose_r', methods=['GET'])
-def combinations_n_choose_r():
+def combinations__n_choose_r():
     n: int = int(request.args.get('n'))
     r: int = int(request.args.get('r'))
 
     result: int = n_choose_r(n, r)
-    result_data_type = type(result).__name__
 
-    data_object: dict = {
-        'data_type': result_data_type,
-        'result': result,
-    }
+    return build_data_object(result)
 
-    return data_object
+
+@app.route('/combinations/combinations_r', methods=['GET'])
+def combinations__combinations_r():
+    n: int = int(request.args.get('n'))
+    r: int = int(request.args.get('r'))
+
+    result: int = n_choose_r(n, r)
+
+    return build_data_object(result)
