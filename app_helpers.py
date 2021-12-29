a42b0e4fce80ec0e46e3da63d01d77ba958fad95
Rick Gladwin <rick@iddesign.ca>
2021-12-29 01:33:44 -0700

    test string json object helpers

--- app_helpers.py
+++ app_helpers.py
@@ -1,17 +1,32 @@
+import json
 import re
 
 
 # helpers for interpreting/handling routes and request data
 
-def string_object_to_object(string_object: str) -> dict:
-    # TODO: if is string object, convert to dict
-    return {"key1":"value 1"}
+def string_object_to_dict(string_object: str) -> dict:
+    # TODO: convert to dict
+    if not is_string_object(string_object):
+        raise ValueError('string_object_to_dict requires a string object')
+
+    return json.loads(string_object)
+
+
+def string_json_object_to_dict(string_object: str) -> dict:
+    if not is_string_json_object(string_object):
+        raise ValueError('string_object_to_dict requires a string object')
+
+    return json.loads(string_object)
 
 
 def is_string_object(source_string: str) -> bool:
-    # TODO: use a regex to determine object/POJO/JSON format
-    # non_json_object_regex: str = '{( ?.+: ?[\'\"]?.+[\'\"]?,?)+}'
-    optional_json_object_regex: str = '{( ?[\'\"]?.+[\'\"]? ?: ?[\'\"]?.+[\'\"]?,? ?)+}'
+    # TODO: extend to nested objects (Note: you did this in node for CIV Team)
+    optional_json_object_regex: str = '^{( ?[\'\"]?.+[\'\"]? ?: ?[\'\"]?.+[\'\"]?,? ?)+}$'
     result = re.search(optional_json_object_regex, source_string)
     return result is not None
-    # return len(result.group(0)) > 0
+
+
+def is_string_json_object(source_string: str) -> bool:
+    json_object_regex: str = '^{( ?\".+\" ?: ?[\'\"]?.+[\'\"]?,? ?)+}$'
+    result = re.search(json_object_regex, source_string)
+    return result is not None
